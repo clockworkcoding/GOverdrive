308f953c06cb82f999fdea337992e232da52e063
clockworkcoding <yodadamax@gmail.com>
2017-07-03 22:54:23 -0500

    Correcting Oauth

--- client.go
+++ client.go
@@ -1,13 +1,8 @@
 package goverdrive
 
 import (
-	"context"
 	"errors"
-	"fmt"
-	"log"
 	"net/http"
-
-	"golang.org/x/oauth2"
 
 	"github.com/mrjones/oauth"
 )
@@ -18,34 +13,6 @@ type Client struct {
 	user        *oauth.AccessToken
 	client      *http.Client
 	consumerKey string
-}
-
-func GetOAuthURL(clientID, clientSecret, libraryAccountId, code, redirectURI string) (token *oauth2.Token) {
-	ctx := context.Background()
-	conf := &oauth2.Config{
-		ClientID:     clientID,
-		ClientSecret: clientSecret,
-		RedirectURL:  redirectURI,
-		Scopes:       []string{libraryAccountId},
-		Endpoint: oauth2.Endpoint{
-			AuthURL:  AuthURL,
-			TokenURL: TokenURL,
-		},
-	}
-
-	// Use the authorization code that is pushed to the redirect
-	// URL. Exchange will do the handshake to retrieve the
-	// initial access token. The HTTP Client returned by
-	// conf.Client will refresh the token as necessary.
-	if _, err := fmt.Scan(&code); err != nil {
-		log.Fatal(err)
-	}
-	tok, err := conf.Exchange(ctx, code)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	return tok
 }
 
 //NewClient is the constructor with only the Consumer key and secret
